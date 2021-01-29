29eba0811e1da5424780a4a6e445c2bb612eb9c3
Rima Akter <rimawahid3@gmail.com>
2021-01-29 16:45:57 +0600

    done

--- calculator.js
+++ calculator.js
@@ -79,3 +79,15 @@ function compute(){
     ans = Math.floor(+eval(input_var.value));
     document.getElementById('answer').value = '=' + ans
 }
+
+var i =0;
+function brackets(){
+    var input_var = document.getElementById('input');
+    if(i == 0){
+        input_var.value += '(';
+        i = 1;
+    }else if(i == 1){
+        input_var.value += ')';
+        i = 0;
+    }
+}
